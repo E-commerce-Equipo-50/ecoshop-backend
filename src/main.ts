b9f6cf46f8bed422520b67bc536b015f00066c30
Maidana <franco_maidana07@hotmail.com>
2025-11-18 20:19:28 -0300

    implementacion de swagger  y borrando tests que no implementaremos (por ahora)

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
   app.setGlobalPrefix(configService.get<string>('app.apiPrefix') || 'api');
-  
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -17,13 +18,22 @@ async function bootstrap() {
       transformOptions: { enableImplicitConversion: true },
     }),
   );
-  
+
   app.enableCors({
     origin: configService.get<string>('app.cors.origin') || '*',
     credentials: configService.get<boolean>('app.cors.credentials') || true,
     optionsSuccessStatus: 200,
   });
-  
+
+  const config = new DocumentBuilder()
+    .setTitle('EcoShop API')
+    .setDescription('API de e-commerce sostenible')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, documentFactory);
+
   const port = configService.get<number>('app.port') || 3000;
   await app.listen(port);
 }
