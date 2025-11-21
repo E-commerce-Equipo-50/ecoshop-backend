9112904c17c0a50106f537ecda9cb91ade638d6b
Mayra Yojhana Muñetón Villegas <johanavillegas.904@gmail.com>
2025-11-21 15:37:35 -0300

    adicionales

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,20 +3,37 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { SellersModule } from './sellers/sellers.module';
+import { CustomersModule } from './customers/customers.module';
 import { HealthController } from './check/health/health.controller';
+import { ProductModule } from './product/product.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
+      isGlobal: true,
+    }),
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        global: true,
+        secret: configService.get<string>('JWT_SECRET') || 'jwt_default_secret',
+        signOptions: { expiresIn: '24h' },
+      }),
+      inject: [ConfigService],
     }),
     MongooseModule.forRoot(process.env.MONGO_DB_URI ?? ''),
     AuthModule,
-    UsersModule,
+    // Modulo para flujo de SELLER (marcas)
+    SellersModule,
+    CustomersModule,
+    ProductModule,
   ],
   controllers: [AppController, HealthController],
   providers: [AppService],
 })
 export class AppModule {}
+
+console.log('MONGODB_URI:', process.env.MONGO_DB_URI);
