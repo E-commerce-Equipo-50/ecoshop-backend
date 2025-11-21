c95c725812e6566d948609ec36a6c861eb194522
Mayra Yojhana Muñetón Villegas <johanavillegas.904@gmail.com>
2025-11-21 20:57:29 -0300

    Otros edits

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -22,6 +22,7 @@ import { RolesGuard } from './guards/roles.guard';
         secret: configService.get<string>('JWT_SECRET') ?? 'jwt_default_secret',
         signOptions: { expiresIn: '24h' },
       }),
+      inject: [ConfigService],
     }),
     PassportModule.register({ defaultStrategy: 'jwt' }),
     CustomersModule,
